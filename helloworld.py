6070b3053e71ec5fd77a2d9ef851875638e3c036
KooperWin <jesus.silva@potros.itson.edu.mx>
2021-11-02 23:03:50 +0000

    primer comment

--- helloworld.py
+++ helloworld.py
@@ -15,3 +15,17 @@ givenString = "ABCDE JESUS */#%$"
 var = givenString[0]
 for i in range(0,len(givenString)):
     print(ord(givenString[i]),',',end='')
+
+
+print()
+
+print('primos')
+for i in range(2, 100): 
+    for j in range(2,i): 
+        if(i % j==0): 
+            break
+    else: 
+        print(i,',',end='')
+
+print()
+            
