5212887340d8e48002a9b7c7e13791e4e36da49a
KooperWin <jesus.silva@potros.itson.edu.mx>
2021-11-02 22:41:44 +0000

    programa impares y ascii

--- helloworld.py
+++ helloworld.py
@@ -5,9 +5,13 @@ for number in range(0,100):
   
 print()
 print('impares')
-for number in range(0,100):
-    if not number%2 == 0:
-        print(number,',',end='')
-        
+for number in range(1,100,2):
+       print(number,',',end='')
 
 print()
+
+print('ascii')
+givenString = "ABCDE JESUS */#%$"
+var = givenString[0]
+for i in range(0,len(givenString)):
+    print(ord(givenString[i]),',',end='')
