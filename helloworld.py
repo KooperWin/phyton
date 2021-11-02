cfa848d2e2dc45c6a345a26ddb9d882bb7c5073b
KooperWin <jesus.silva@potros.itson.edu.mx>
2021-11-02 22:10:37 +0000

    subir pares

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+print('pares')
+for number in range(0,100):
+    if number%2 == 0:
+        print(number,',',end='')
+  
+print()
+print('impares')
+for number in range(0,100):
+    if not number%2 == 0:
+        print(number,',',end='')
+        
+
+print()
